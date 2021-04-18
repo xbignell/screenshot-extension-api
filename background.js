171db384eb3f55843355a0e3c3f9640caa48eacb
Xavi <xavierbignell@gmail.com>
2021-04-18 14:10:27 +0200

    Do screenshot from event

--- background.js
+++ background.js
@@ -1,1 +1,8 @@
-chrome.runtime.onInstalled.addListener(() => {});
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    chrome.tabs.captureVisibleTab(null,{ "format": "png" },async (dataUri) => {
+       chrome.tabs.executeScript({
+           code: `const image = document.getElementById('image'); 
+           image.src = "${dataUri}"`
+       })
+    });
+});
--- take-screenshot.js
+++ take-screenshot.js
@@ -1,14 +1,3 @@
-let screenshot = document.getElementById("screenshot");
-
-screenshot.addEventListener("click", async () => {
-    chrome.tabs.captureVisibleTab(null,{"format":"png"},function(dataUri) {
-        download(dataUri);
-    });
-})
-
-const download = (data) => {
-    const fileElement = document.createElement('a');
-    fileElement.href = data;
-    fileElement.download = 'reddot.png';
-    fileElement.click();
-}
+document.addEventListener('takeScreenshot', () => {
+    chrome.runtime.sendMessage({}, () => {});
+}, false);
